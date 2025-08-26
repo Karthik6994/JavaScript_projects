113248de34fa2b96533ce7eecf038260e4bb01b3
Karthik <159438219+Karthik6994@users.noreply.github.com>
2025-08-26 23:13:08 +0530

    Add files via upload

---
+++ dark and light theme/script.js
@@ -0,0 +1,13 @@
+let  container = document.body
+let cur_theme = localStorage.getItem("key_theme")
+container.classList = cur_theme
+function switchTheme(){
+    if(localStorage.getItem("key_theme")){
+        console.log("...",cur_theme)
+        container.classList="" 
+        localStorage.removeItem("key_theme")
+    }else{
+        localStorage.setItem("key_theme","theme")
+        container.classList="theme"
+    }
+   }
